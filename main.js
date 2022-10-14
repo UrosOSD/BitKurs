664f60850f7178cc53c61c18eccc44b5ac9af034
BIT Student <classroom@bgit.rs>
2022-10-14 14:52:49 +0200

    add new files

--- main.js
+++ main.js
@@ -1,6 +1,139 @@
-var a = 5
-var b = 10
 
-console.log(a*b);
+// var a = 9.3;
+          
+// if (a % 2 == 1) {
+//     console.log('odd');
+// } else if (a % 2 == 0) {
+//     console.log('even');
+// } else {
+//     console.log('Nije ceo broj')
+// }
 
-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
+
+
+
+
+
+
+// if (b%3==0 && b%5==0) {
+
+//         console.log(b);
+//     }
+//   else {console.log('Broj nije deljiv sa 3 i 5');}  
+
+
+// b=15
+// if (b%3==0) {
+//     if (b%5==0) {
+//     console.log('b='+b);    
+//     }    
+// }
+
+// var q=3;
+// var w=7;
+// var r=2;
+
+// var proizvod = q*w*r;
+
+// if (proizvod<0) {
+//     console.log('-');
+    
+// } else if (proizvod>0) {
+// console.log('+');
+// }
+
+
+// var a = 10;
+
+// if (typeof a == 'number' && a%2 == 0) {
+//     console.log(a);
+// } else {
+//     console.log('X');
+// }
+
+
+
+// z = 1001
+// if (typeof z == 'number' && z%2===0) {
+//     console.log(z);
+    
+// } else console.log('x');
+
+
+
+// var a1=500
+// var a2=200
+// var a3=60
+// var a4=0
+// var a5=11
+
+// var max;
+// if (a1>=a2 && a1>=a3 && a1>=a4 && a1>=a5) {
+//     max=a1
+// } else if (a2>=a3 && a2>=a4 && a2>=a5) {
+//     max=a2    
+// } else if (a3>=a4 && a3>=a5) {
+//     max=a3
+// } else if (a4>=a5) {
+//     max=a4
+// } else {
+//     max=a5
+// }
+// console.log(max);
+
+
+// s1=10000
+// s2=10000000
+// s3=400000
+
+// m2=s2
+// m1=(s1>s2)?s1:s2
+// m1=(m1>s3)?m1:s3
+
+// m3=(s3<s1)?s3:s1
+// m3=(m3<s2)?m3:s2
+
+// m2=(s1<m1 && s1>m3)?s1:(s2<m1 && s2>m3)?s2:s3
+
+// if (s1<m1 && s1>m3) {
+//         m2=s1
+//     } else if (s2<m1 && s2>m3) {
+//         m2=s2
+//     } else {
+//         m2=s3
+//     }
+
+// console.log(m1,m2,m3);
+
+// if (s1<m1 && s1>m3 || s1>m1 && s1<m3) {
+//     m2=s1
+// } else if (s2<m1 && s2>m3 || s2>m1 && s2<m3) {
+//     m2=s2
+// } else if (s3<m1 && s3>m3 || s3>m1 && s3<m3) {
+//     m2=s3
+// }
+
+
+// var a = 20;
+
+// if (a % 3 == 0 && a % 5 == 0){
+//     console.log('FizzBuzz')
+// }
+
+// else if (a % 3 == 0) {
+//     console.log('Fizz');
+// } else if (a % 5 == 0) {
+//     console.log('Buzz');
+// } 
+
+// else {
+//     console.log('Nije deljiv ni sa kim');
+// }
+
+
+var a = 11
+
+if (a > 13) 
+    console.log((a-13)*2);
+else 
+    console.log(13-a);
